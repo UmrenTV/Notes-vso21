d7e4638da900a801681b39c86e3c24043227071f
UmrenTV <zdravevski.jane@gmail.com>
2023-04-21 12:58:18 +0200

    initial

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,8 +1,115 @@
+<!-- src/views/SignIn.vue -->
 <script>
-export default {}
+import { $http } from "../utils/http";
+
+export default {
+  data() {
+    return {
+      signInEmail: "",
+      signInPassword: "",
+      emailCheckResult: "",
+    };
+  },
+  methods: {
+    signIn() {
+      // Send request to the backend
+      $http;
+      // code here
+    },
+    checkEmail() {
+      // If the email is empty, don't send a request
+      if (!this.signInEmail) {
+        this.emailCheckResult = "";
+        return;
+      }
+
+      // Send a POST /users/check-email request to the backend
+      $http
+        .post(
+          "/users/check-email",
+          {
+            email: this.signInEmail,
+          },
+          { disableErrorHandling: true }
+        )
+        .then((response) => {
+          this.emailCheckResult = "";
+        })
+        .catch((response) => {
+          this.emailCheckResult = response.body.error;
+        });
+    },
+  },
+};
 </script>
+
 <template>
   <div>
-    <h1>Sign In</h1>
+    <div>
+      <h1>Sign In</h1>
+
+      <!-- Email -->
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+          v-on:keyup="checkEmail"
+          v-model="signInEmail"
+        />
+        <label class="label">
+          <!-- Show red text if the email is already taken -->
+          <span
+            class="label-text-alt text-red-600"
+            id="email-error"
+            :class="{ invisible: emailCheckResult.length === 0 }"
+          >
+            {{ emailCheckResult }}.
+          </span>
+        </label>
+      </div>
+
+      <!-- Password -->
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">Password</span>
+        </label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Type here"
+          class="input input-bordered w-full max-w-xs"
+          v-model="signInPassword"
+        />
+        <label class="label">
+          <!-- Show red text if the password is too short -->
+          <span
+            class="label-text-alt text-red-600"
+            id="password-error"
+            :class="{
+              invisible: !(
+                signInPassword.length > 0 && signInPassword.length < 8
+              ),
+            }"
+            >Password must be at least 8 characters long</span
+          >
+        </label>
+      </div>
+    </div>
+    <div class="h-30">&nbsp;</div>
+    <button id="sign-in" class="btn btn-primary" @click="signIn">
+      Sign In
+    </button>
+    <!-- Added this div to prevent the error "only one root element from Vue2" -->
   </div>
 </template>
+
+<style>
+.invisible {
+  visibility: hidden;
+}
+</style>
